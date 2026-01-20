fa471cc7f6bbb2fdf9c56b95ee8ad67b518d1d72
adamhamri9 <adamhamri9@proton.me>
2026-01-20 23:27:55 +0100

    feat(api): integrate scalar docs and refine search endpoint
    - Add /scalar endpoint for interactive API documentation using Scalar.
    
    - Update /search endpoint to use SearchResponse as the response model for better schema definition.

--- api/main.py
+++ api/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
+from scalar_fastapi import get_scalar_api_reference
 from api.schemas.search_input import SearchInput
+from api.schemas.search_response import SearchResponse
 from api.services.search.retriever import QuranRetriever
 from api.services.search.context import QuranContextBuilder
 from api.services.search.service import search
+
 
 app = FastAPI(title="Quran Semantic Search API")
 
@@ -18,7 +21,7 @@ app.add_middleware(
 retriever = QuranRetriever()
 context_builder = QuranContextBuilder(retriever.verses)
 
-@app.post("/search")
+@app.post("/search", response_model=SearchResponse)
 async def search_quran(params: SearchInput):
     try:
         result = await search(retriever, context_builder, params)
@@ -27,3 +30,13 @@ async def search_quran(params: SearchInput):
         raise HTTPException(status_code=400, detail=str(ve))
     except Exception:
         raise HTTPException(status_code=500, detail="Internal server error")
+
+
+@app.get("/scalar", include_in_schema=False)
+async def scalar_html():
+    return get_scalar_api_reference(
+        # Your OpenAPI document
+        openapi_url=app.openapi_url,
+        # Avoid CORS issues (optional)
+        scalar_proxy_url="https://proxy.scalar.com",
+)
